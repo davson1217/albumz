b32f3938f10a18b324dc2c72a6735adc277a83d9
David <olurebidavid@gmail.com>
2019-02-02 00:38:17 +0400

    Sign in / Sign  Up page added

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Albumz</title>
+</head>
+<body>
+
+		<form method="POST" onsubmit="return validateForm()">
+			<label for="user">Username</label>
+			<input type="type" name="user">
+			<br>
+			<label for="pass">Password</label>
+			<input type="password" name="pass">	
+		</form>
+
+</body>
+</html>
